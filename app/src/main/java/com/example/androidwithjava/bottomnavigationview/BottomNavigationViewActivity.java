e08cf2b83a357f62e904b32bc6db1ce4bdad76ca
mansukh makwana <mansukh.m@celcius.in>
2026-09-09 18:58:20 +0530

    09-09-2026 - navigation drawer

--- app/src/main/java/com/example/androidwithjava/bottomnavigationview/BottomNavigationViewActivity.java
+++ app/src/main/java/com/example/androidwithjava/bottomnavigationview/BottomNavigationViewActivity.java
@@ -1,20 +1,33 @@
 package com.example.androidwithjava.bottomnavigationview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.window.OnBackInvokedDispatcher;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.androidwithjava.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class BottomNavigationViewActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,49 @@ public class BottomNavigationViewActivity extends AppCompatActivity {
         });
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        //step 1
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.OpenDrawer, R.string.CloseDrawer);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int menuItem = item.getItemId();
+                if (menuItem == R.id.drawer_home) {
+                    loadFragment(new HomeFragment());
+
+                } else if (menuItem == R.id.drawer_search) {
+                    loadFragment(new SearchFragment());
+
+                } else if (menuItem == R.id.drawer_contact) {
+                    loadFragment(new ContactusFragment());
+
+                } else if (menuItem == R.id.drawer_settings) {
+                    loadFragment(new UtilitiesFragment());
+
+                } else if (menuItem == R.id.drawer_profile) {
+                    loadFragment(new ProfileFragment());
+
+                } else if (menuItem == R.id.drawer_utilities) {
+                    loadFragment(new UtilitiesFragment());
+
+                } else if (menuItem == R.id.drawer_logout) {
+                    loadFragment(new HomeFragment());
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
 
@@ -58,12 +114,28 @@ public class BottomNavigationViewActivity extends AppCompatActivity {
             bottomNavigationView.setSelectedItemId(R.id.nav_profile);
         }
 
+        getOnBackPressedDispatcher().addCallback(
+                this,
+                new OnBackPressedCallback(true) {
+
+                    @Override
+                    public void handleOnBackPressed() {
+
+                        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                            drawerLayout.closeDrawer(GravityCompat.START);
+
+                        } else {
+                            setEnabled(false);
+                            getOnBackPressedDispatcher().onBackPressed();
+                        }
+                    }
+                }
+        );
+
     }
 
     private void loadFragment(Fragment fragment) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
     }
+
 }
